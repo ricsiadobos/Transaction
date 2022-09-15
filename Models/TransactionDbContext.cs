e430aa1451228766ac1f468e4d2c00315e79d779
ricsiadobos <ricsirendel@gmail.com>
2022-09-16 00:07:40 +0200

    First push

---
+++ Models/TransactionDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AspCoreCRUD.Models
+{
+    public class TransactionDbContext: DbContext
+    {
+        public TransactionDbContext(DbContextOptions<TransactionDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
